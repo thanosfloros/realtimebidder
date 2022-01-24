2463188b3109cba669eb0b560c74ceca775f1c9e
Athanasios Floros <thanosfloros@gmail.com>
2022-01-24 17:33:25 +0200

    fix exception

--- src/main/java/com/projectagora/InvalidCountryException.java
+++ src/main/java/com/projectagora/InvalidCountryException.java
@@ -3,7 +3,9 @@ package com.projectagora;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Invalid country")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Invalid country")
 public class InvalidCountryException extends Exception {
-
+    public InvalidCountryException() {
+        super("Invalid country");
+    }
 }
